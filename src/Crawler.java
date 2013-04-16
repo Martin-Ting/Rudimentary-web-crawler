f0b0f830bc9f5c7014a34ef3c434bcf3c1523147
Martin Ting <imnotmartin@gmail.com>
2013-04-15 22:38:31 -0700

    Project Files added.

---
+++ src/Crawler.java
@@ -0,0 +1,8 @@
+/**
+ * File: Crawler.java
+ * Description: Contains the crawler class
+ * User: imnotmartin
+ * Date: 4/15/13
+ */
+public class Crawler {
+}
---
+++ src/Parser.java
@@ -0,0 +1,9 @@
+/**
+ * File: Parser.java
+ * Description: Contains the parsing class
+ * User: imnotmartin
+ * Date: 4/15/13
+ */
+public class Parser {
+
+}
